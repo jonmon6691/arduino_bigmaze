588aea9a047d19161d87b106a45105831e108fa7
jonmon6691 <jonmon6691@gmail.com>
2020-01-18 16:31:51 -0800

    Added comments to make_levels_h.py

--- make_levels_h.py
+++ make_levels_h.py
@@ -10,6 +10,8 @@ f.write("#define LEVELS_H\n\n")
 
 f.write(f"#define N_LEVELS {len(levels)}\n\n")
 
+# Level data is kept in one long byte array.
+# Meta information on how to access it is stored in 'struct level levels[]', defined at the bottom
 f.write("const uint8_t level_data[] PROGMEM = {\n")
 
 byte_counter = 0
@@ -20,11 +22,14 @@ for level_num, (w, h, data) in enumerate(levels, start=1):
     #only keep east and south bits
     data = [((c&0b1000)>>2)|((c&0b10)>>1) for c in data]
 
+    # Make a helpful comment in the header file about this level
     f.write(f"// Level {level_num} - {w}x{h} (offset {byte_counter})\n")
     offsets.append(byte_counter)
 
     for row in chunker(data, w):
         f.write("\t")
+        # Since each maze cell only takes 2 bits to represent (can go east, can go south),
+        # we can pack 4 of them into a single byte.
         for cell_chunk in chunker(row, 4):
             b = 0
             for i in range(min(len(cell_chunk), 4)):
@@ -45,8 +50,10 @@ struct level {
 struct level levels[] = {
 """)
 
+# Tells the code how to access the level information out of the flat store in level_data[]
 for (w, h, data), offset in zip(levels, offsets):
     row_size = ceil(len(data) / h / 4)
     f.write(f"\t{{ .w = {w}, .h = {h}, .row_size = {row_size}, .level_offset = {offset}}},\n")
 
 f.write("};\n\n#endif // ifnedef levels_h")
+f.close()
